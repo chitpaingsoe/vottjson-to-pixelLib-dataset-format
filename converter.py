407f20882aa13f939d42dab22dc616dadea12693
cpsoe <LENOVO@DESKTOP-36Q9SL4>
2020-10-26 16:34:56 +0630

    fix no regions in asset

--- converter.py
+++ converter.py
@@ -35,53 +35,56 @@ if len(sys.argv) > 2:
             regions = assets[value]["regions"]
             imagePath = info["path"].replace("file:","")
             
-            #Create each image jsonfile           
+            if len(regions) > 0:
 
-            data["version"] = "4.5.6"
-            data["flags"] = {}
-            data["shapes"] = []
-            data["imagePath"] = info["name"]
-            data["imageHeight"] = info["size"]["width"]
-            data["imageWidth"] = info["size"]["height"]
-
-            #set Region
-            for region in regions:
-                region_item = {}
-                region_item["points"] = []
-                region_item["label"] =   region["tags"][0] if len(region["tags"]) > 0 else ""
-                region_item["group_id"] = None
-                region_item["shape_type"] = "polygon"
-                region_item["flags"] = {}
-
-                for point in region["points"]:
-                    item = [point["x"],point["y"]]
-                    region_item["points"].append(item)
-                
-                data["shapes"].append(region_item)
-            #image Data
-            with open(imagePath, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-                imageData = encoded_string.decode("utf-8")
-                data["imageData"] = imageData
-                
-            #write json file
-            fileName = ""
-            if key < training_index:
-                if os.path.exists(train_dir):
-                    fileName = train_dir + "/" + info["name"].replace(".jpg",".json")
-                    copyImageFileName = train_dir + "/" + info["name"]
-                    copyfile(imagePath,copyImageFileName)
-            else:
-                if os.path.exists(test_dir):
-                    fileName = test_dir + "/" + info["name"].replace(".jpg",".json") 
-                    copyImageFileName = test_dir + "/" + info["name"]
-                    copyfile(imagePath,copyImageFileName)                   
             
-            # Serializing json  
-            json_object = json.dumps(data, indent = 4) 
-            with open(fileName, "w") as outfile: 
-                outfile.write(json_object) 
-            print("Write To ",fileName)
+                #Create each image jsonfile           
+
+                data["version"] = "4.5.6"
+                data["flags"] = {}
+                data["shapes"] = []
+                data["imagePath"] = info["name"]
+                data["imageHeight"] = info["size"]["width"]
+                data["imageWidth"] = info["size"]["height"]
+
+                #set Region
+                for region in regions:
+                    region_item = {}
+                    region_item["points"] = []
+                    region_item["label"] =   region["tags"][0] if len(region["tags"]) > 0 else ""
+                    region_item["group_id"] = None
+                    region_item["shape_type"] = "polygon"
+                    region_item["flags"] = {}
+
+                    for point in region["points"]:
+                        item = [point["x"],point["y"]]
+                        region_item["points"].append(item)
+                
+                    data["shapes"].append(region_item)
+                #image Data
+                with open(imagePath, "rb") as image_file:
+                    encoded_string = base64.b64encode(image_file.read())
+                    imageData = encoded_string.decode("utf-8")
+                    data["imageData"] = imageData
+                
+                #write json file
+                fileName = ""
+                if key < training_index:
+                    if os.path.exists(train_dir):
+                        fileName = train_dir + "/" + info["name"].replace(".jpg",".json")
+                        copyImageFileName = train_dir + "/" + info["name"]
+                        copyfile(imagePath,copyImageFileName)
+                else:
+                    if os.path.exists(test_dir):
+                        fileName = test_dir + "/" + info["name"].replace(".jpg",".json") 
+                        copyImageFileName = test_dir + "/" + info["name"]
+                        copyfile(imagePath,copyImageFileName)                   
+            
+                # Serializing json  
+                json_object = json.dumps(data, indent = 4) 
+                with open(fileName, "w") as outfile: 
+                    outfile.write(json_object) 
+                print("Write To ",fileName)
 
         jsonFile.close()
     except IOError:
